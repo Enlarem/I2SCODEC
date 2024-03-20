6b469450e964468b31cf0fde0a1412f8adeba906
Enlarem <remi.laramas@gmail.com>
2024-03-20 14:34:01 +0200

    Unpacked AudioCodecDemo archive

--- tcl/2-synth.tcl
+++
@@ -1,7 +0,0 @@
-## Run synthesis
-synth_design -top gameStateMachine -part $part -flatten_hierarchy rebuilt
-write_checkpoint -force $outdir/post_synth
-# Create simulation sources
-write_vhdl -mode funcsim -force $outdir/gameStateMachine_funcsim_post_synth.vhd
-write_verilog -mode timesim -force $outdir/gameStateMachine_timesim_post_synth.v
-
