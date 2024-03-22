3b4f12e10e727a0f44267532f1dd3d318e2993df
Enlarem <remi.laramas@gmail.com>
2024-03-22 15:50:56 +0200

    Working script for linux, need testing for windows

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,27 @@
 #! /bin/sh
 
+if [ $# -gt 2 ] ; then
+    echo "[Error] : Need to provide the component name to run the simulation of"
+    exit 1
+fi
+
+if [ -z $1 ] ; then
+    echo "[Error] : Need to provide the component name to run the simulation of"
+    exit 1
+fi
+
+echo "[Pre-implementation] Updates list of files..."
+FILES=$(find ../src/ -name "*.vhd")
+echo $FILES
+echo "" > "top-tb.prj"
+for f in $FILES; do
+    echo "vhdl xil_defaultlib \"$f\" " >> "top-tb.prj"
+done
+
+echo "[Pre-implementation] Files updated !"
+
 #Build the simulation
-xelab -prj top-tb.prj -s top-tb_simulation_snapshot xil_defaultlib.top_tb --nolog -debug all
+xelab -prj top-tb.prj -s "$1_simulation_snapshot" "xil_defaultlib.$1" --nolog -debug all
+
 #Run the simulation
-xsim top-tb_simulation_snapshot -gui -wdb simulate_xsim.wdb --nolog
+xsim "$1_simulation_snapshot" -gui -wdb simulate_xsim.wdb -view "Waveforms/basic_config.wcfg" --nolog
--- scripts/run_funcsim.bat
+++ scripts/run_funcsim.bat
@@ -5,11 +5,37 @@
 @ECHO OFF
 
 @REM  Configure the path to run the Vivado Design Suite
-CALL C:\Xilinx\Vivado\2020.2\settings64.bat
+CALL C:\Xilinx\Vivado\2023.2\settings64.bat
 
 @REM Define Xilinx Vivado environment variable
-SET XILINX_VIVADO=C:\Xilinx\Vivado\2020.2
+SET XILINX_VIVADO=C:\Xilinx\Vivado\2023.2
+
+setlocal EnableDelayedExpansion
+
+if "%~2" neq "" (
+    echo [Error] : Need to provide the component name to run the simulation of
+    exit /B 1
+)
+
+if "%~1"=="" (
+    echo [Error] : Need to provide the component name to run the simulation of
+    exit /B 1
+)
+
+echo [Pre-implementation] Updates list of files...
+set "FILES="
+for /R ..\src %%f in (*.vhd) do (
+    set "FILES=!FILES! "%%f""
+)
+echo !FILES!
+echo. > "top-tb.prj"
+
+for %%f in (!FILES!) do (
+    echo vhdl xil_defaultlib "%%f" >> "top-tb.prj"
+)
+
+echo [Pre-implementation] Files updated !
 
 CALL xvhdl --nolog -prj top-tb.prj 
-CALL xelab xil_defaultlib.top_tb -prj top-tb.prj -debug all --nolog
-CALL xsim --nolog -g -view xil_defaultlib.top_tb.wcfg xil_defaultlib.top_tb
+CALL xelab xil_defaultlib."%~1" -prj top-tb.prj -debug all --nolog
+CALL xsim --nolog -g -view Waveforms\basic_config.wcfg xil_defaultlib."%~1"
