3c995d5a9a2286b4afb016c196e10bb4b2d12bbc
Enlarem <remi.laramas@gmail.com>
2024-03-26 09:24:07 +0100

    Batch script should be working

--- scripts/run_funcsim.bat
+++ scripts/run_funcsim.bat
@@ -5,15 +5,16 @@
 @ECHO OFF
 
 @REM  Configure the path to run the Vivado Design Suite
-CALL C:\Xilinx\Vivado\2023.2\settings64.bat
+@REM CALL C:\Xilinx\Vivado\2023.2\settings64.bat
 
 @REM Define Xilinx Vivado environment variable
-SET XILINX_VIVADO=C:\Xilinx\Vivado\2023.2
-
-setlocal enabledelayedexpansion
-
+@REM  SET XILINX_VIVADO=C:\Xilinx\Vivado\2023.2
 @echo off
 setlocal enabledelayedexpansion
+
+(set LF=^
+%=DO NOT REMOVE THIS=%
+)
 
 if "%~1" == "" (
     echo [Error] : Need to provide the component name to run the simulation of
@@ -28,8 +29,7 @@ if not "%~2" == "" (
 echo [Pre-implementation] Updates list of files...
 set "FILES="
 for /R ..\src %%f in (*.vhd) do (
-    set "FILES=!FILES!
-    vhdl xil_defaultlib '%%f' "
+    set "FILES=!FILES!!LF!vhdl xil_defaultlib '%%f' "
 )
 
 echo.> "top-tb.prj"
@@ -39,8 +39,8 @@ echo [Pre-implementation] Files updated !
 
 echo [Implementation] Building the simulation
 cd ..\simulations
-xelab -prj ..\scripts\top-tb.prj -s "%~1_simulation_snapshot" "xil_defaultlib.%~1" --nolog -debug all
+@REM xelab -prj ..\scripts\top-tb.prj -s "%~1_simulation_snapshot" "xil_defaultlib.%~1" --nolog -debug all
 
 echo [Implementation] Running the simulation
 cd ..\scripts
-xsim "%~1_simulation_snapshot" -gui -wdb "..\simulations\simulate_xsim.wdb" -view "Waveforms\basic_config.wcfg" --nolog --xsimdir ..\simulations
+@REM xsim "%~1_simulation_snapshot" -gui -wdb "..\simulations\simulate_xsim.wdb" -view "Waveforms\basic_config.wcfg" --nolog --xsimdir ..\simulations
