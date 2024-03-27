d1b3369447c477d0c55f0d5d50235d96b72f4daf
KrizAntonin <antoninkriz6@gmail.com>
2024-03-27 17:49:25 +0200

    master clk in I2S

--- src/vhdl/i2s_layer.vhd
+++ src/vhdl/i2s_layer.vhd
@@ -8,6 +8,7 @@ entity i2s_layer is
   filter_mode : in std_logic;
 
     --Input ports
+    sysclk    : in std_logic;
     ac_bclk   : in std_logic;
     ac_muten  : in std_logic;
     ac_pblrc  : in std_logic;
@@ -23,6 +24,26 @@ entity i2s_layer is
 end i2s_layer;
 architecture Behavioral of i2s_layer is
 
+signal mclk : std_logic := '0';--25 Mhz master clock 
+
 begin
+
+process (sysclk)
+  constant cntm_max : integer := 20 -- 20 rising edges, half a periode
+  variable cntm : integer range 0 to cntm_max :=0;
+
+begin
+  if rising_edge(sysclk) then
+    if cntm < cntm_max - 1 then
+      cntm := cnt + 1;
+    else 
+      cnt := 0;
+      mclk <= not mclk;
+    end if ;
+  end if;
+end process;
+
+
+
 
 end Behavioral; -- Behavioral
