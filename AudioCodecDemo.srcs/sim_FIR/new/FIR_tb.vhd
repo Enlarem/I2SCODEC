087f691200c653e97a6b2347cf820704f2392759
KrizAntonin <antoninkriz6@gmail.com>
2024-04-17 10:09:40 +0300

    FIR testbench
    not working yet

--- AudioCodecDemo.srcs/sim_FIR/new/FIR_tb.vhd
+++ AudioCodecDemo.srcs/sim_FIR/new/FIR_tb.vhd
@@ -1,6 +1,8 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
+use std.textio.all;
+use ieee.std_logic_textio.all;
+use IEEE.Numeric_std.all;
 
 
 entity FIR_tb is
@@ -10,9 +12,7 @@ architecture Behavioral of FIR_tb is
 
 component FIR
     Generic (width      : integer := 24;
-            taps        : integer := 17;
-            in_width    : integer range 8 to 32 := 24;
-            out_width   : integer range 8 to 32 := 24;
+            taps        : integer := 19;
             coef_width  : integer range 8 to 32 := 16
             );
     Port (  
@@ -22,8 +22,7 @@ component FIR
         ac_reclrc   : in std_logic;
         rec_done    : in std_logic;
         r_buff      : in std_logic_vector((2*width-1) downto 0) := (others => '0');
-        fil_buff    : out std_logic_vector((2*width-1) downto 0) := (others => '0');
-        ac_pbdat    : out std_logic
+        fil_buff    : out std_logic_vector((2*width-1) downto 0) := (others => '0')
             );
 end component;
 
@@ -35,11 +34,120 @@ component clockSim
            reclrc : out STD_LOGIC);
 end component;
 
+    constant mclk_period : integer := 80;
+    constant width : integer := 24;
+    constant taps : integer := 19;
+    constant coef_width : integer := 16;
 
-
+    signal sim_clk, sim_bclk, sim_mclk, sim_reclrc, sim_recdat : std_logic := '0';
+    signal sim_fil_buff, sim_buff, out_buff :  std_logic_vector ((2*width-1) downto 0) := (others => '0');
+    signal sim_done, sim_reset : std_logic := '0';
+ --   signal sim_left : std_logic_vector (width-1 downto 0) := (others => '0');
+ --   signal sim_right : std_logic_vector (width-1 downto 0) := (others => '0');
+    signal recL_buff, recR_buff, firL_buff, firR_buff : std_logic_vector((width-1) downto 0) := (others => '0');
+    file input_buf : text;
 
 
 begin
 
+clocks: clockSim 
+    generic map(
+        mclk_period => mclk_period
+    )
+    port map(
+        mclk => sim_mclk,
+        bclk => sim_bclk,
+        reclrc => sim_reclrc
+    );
+
+uut: FIR
+generic map (width => width,
+            taps => taps,
+            coef_width => coef_width
+            )
+port map ( 
+            sysclk => sim_clk,
+            ac_bclk => sim_bclk,
+            ac_mclk => sim_mclk,
+            ac_reclrc => sim_reclrc,
+            r_buff => sim_buff,
+            rec_done => sim_done,
+            fil_buff => sim_fil_buff
+            );
+
+
+
+CLKprocess : process
+begin
+    wait for mclk_period/20*ns;
+        sim_clk <= not sim_clk;
+end process;
+
+
+
+
+recProcess: process
+begin
+    wait until falling_edge(sim_bclk);
+    for i in 241 downto 0 loop
+        wait until falling_edge(sim_bclk);
+        wait until rising_edge(sim_bclk);
+        wait until falling_edge(sim_bclk);
+        wait until rising_edge(sim_bclk);
+        sim_done <= '1';
+        wait until rising_edge(sim_bclk);
+        sim_done <= '0';
+    end loop;
+end process;
+
+
+
+stimProcess: process
+
+variable inLine : line;
+variable colTmp: character;
+variable l, r, l_fir, r_fir : integer;
+
+    begin
+    
+        file_open(input_buf, "sound_v.csv",  read_mode);
+       -- readline(input_buf, inLine); -- 1.st line has the column names
+        
+        while not endfile(input_buf) loop
+            for i in (241) downto 0 loop
+                readline(input_buf, inLine);
+                read(inLine, l);
+                read(inLine, colTmp);
+                read(inLine, r);
+                read(inLine, colTmp);
+                read(inLine, l_fir);
+                read(inLine, colTmp);
+                read(inLine, r_fir);
+                
+                recL_buff <= std_logic_vector(to_signed(l, recL_buff'length));
+                recR_buff <= std_logic_vector(to_signed(r, recR_buff'length));
+                firL_buff <= std_logic_vector(to_signed(l_fir, firL_buff'length));
+                firR_buff <= std_logic_vector(to_signed(r_fir, firR_buff'length));
+                
+                sim_buff(2*width-1 downto width) <= recL_buff;
+                sim_buff(width-1 downto 0) <= recR_buff;
+                out_buff(2*width-1 downto width) <= firL_buff;
+                out_buff(width-1 downto 0) <= firR_buff;
+                
+                wait on sim_done;
+            end loop;
+            
+            assert((sim_fil_buff = out_buff)) report "error" severity error;   -- add output of the filter     
+            
+        end loop;
+        
+        file_close(input_buf);
+        report "Finished";
+        wait;
+    end process;
 
 end Behavioral;
+
+
+
+--  output_1b <= std_logic_vector(to_signed(input_1, output_1b'length));
